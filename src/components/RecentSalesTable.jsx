3610bc96b841a6af9da455e606d687782d5f5eb8
Michele Marschner <michele.marschner@gmail.com>
2024-09-19 10:33:42 +0200

    optimized table for mobile view

--- src/components/RecentSalesTable.jsx
+++ src/components/RecentSalesTable.jsx
@@ -11,36 +11,34 @@ function RecentSalesTable({ items }) {
     }
 
   return (
-   
-        <table className='table table-borderless datatable'>
-            <thead className='table-light'>
-                <tr>
-                    <th scope='col'>Customer ID</th>
-                    <th scope='col'>Customer</th>
-                    <th scope='col'>Cart ID</th>
-                    <th scope='col'>Price</th>
-                    <th scope='col'>Status</th>
+    <table className='table table-borderless datatable'>
+        <thead className='table-light'>
+            <tr>
+                <th scope='col'>Customer ID</th>
+                <th scope='col'>Customer</th>
+                <th scope='col'>Cart ID</th>
+                <th scope='col'>Price</th>
+                <th scope='col'>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            {items && items.length > 0 && items.map(item => (
+                <tr key={item._id}>
+                    <th scope='row'>
+                        <a href='/'>{item.customerId}</a>
+                    </th>
+                    <td>{item.customer}</td>
+                    <td>
+                        <a href='/'>{item.cartId}</a>
+                    </td>
+                    <td>${item.price.toFixed(2)}</td>
+                    <td>
+                        <span className={`badge bg-${handleStatus(item.status)}`}>{item.status}</span>
+                    </td>
                 </tr>
-            </thead>
-            <tbody>
-                {items && items.length > 0 && items.map(item => (
-                    <tr key={item._id}>
-                        <th scope='row'>
-                            <a href='/'>{item.cartId}</a>
-                        </th>
-                        <td>{item.customer}</td>
-                        <td>
-                            <a href='/'>{item.cartId}</a>
-                        </td>
-                        <td>${item.price.toFixed(2)}</td>
-                        <td>
-                            <span className={`badge bg-${handleStatus(item.status)}`}>{item.status}</span>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    
+            ))}
+        </tbody>
+    </table>
   )
 }
 
